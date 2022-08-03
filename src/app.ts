188bd9dc3212923bbdb85bca04563174d742473e
JakubMizera <kubamiz53@gmail.com>
2022-08-03 16:55:00 +0200

    validation schema with zod ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import config from 'config';
 import connect from './utils/connect';
 import logger from './utils/logger';
+import routes from './routes';
 
 //config folder => default.ts => port => 1337 (value)
 const port = config.get<number>('port')
@@ -15,6 +16,8 @@ app.listen(port, async ()=> {
     logger.info(`App is running at http://localhost:${port}`);
 
     await connect();
+
+    routes(app)    
 });
 
 
