281775dc1731e99a0ac6269c086e286f9310b6f4
JakubMizera <kubamiz53@gmail.com>
2022-08-08 12:57:56 +0200

    created user model & hashing func for password

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,7 @@
 export default {
     port: 1337,
-    dbUri: "mongodb://localhost:27017/rest-api-tutorial"
+    dbUri: "mongodb://localhost:27017/rest-api-tutorial",
+    saltWorkFactor: 10,
 }
+
+// saltWorkFactor = number of times password is being encrypted (the more, the more secure it is)
---
+++ src/models/user.model.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import config from "config";
+
+export interface UserDocument extends mongoose.Document {
+    email: string;
+    name: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword(candidatePassword: string): Promise<Boolean>;
+};
+
+
+// Schema definition
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true, unique: false },
+    password: { type: String, required: true },
+}, {
+    timestamps: true
+});
+
+// password hashing function
+userSchema.pre("save", async function (next) {
+    let user = this as UserDocument;
+
+    //if password is not modified
+    if (!user.isModified("password")) {
+        return next();
+    }
+    //if password is modified, it's being encrypted
+
+    const salt = await bcrypt.genSalt(config.get<number>('saltWorkFactor'));
+    //creating hash to replace password
+    const hash = bcrypt.hashSync(user.password, salt);
+
+    //replacing password with hash
+    user.password = hash;
+
+    return next();
+})
+
+// async func to compare password, takes 1 argument = password to check, returns Promise with boolean type;
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+    const user = this as UserDocument
+
+    return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+}
+
+
+
+// Model of user
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
