a23c68d4140f45e679832a7ca0e048f801ff2270
JakubMizera <kubamiz53@gmail.com>
2022-08-08 13:58:28 +0200

    user schema, user validation

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,20 @@
+import {object, string} from 'zod';
+export const createUserSchema = object({
+    body: object({
+        name: string({
+            required_error: 'Name is required'
+        }),
+        password: string({
+            required_error: 'Password is required'
+        }).min(6, 'Password must be at least 6 chars long'),
+        passwordConfirmation: string({
+            required_error: 'Password confirmation is required'
+        }),
+        email: string({
+            required_error: 'Email is required'
+        }).email('Not a valid email')
+    }).refine((data) => data.password === data.passwordConfirmation, {
+        message: 'Passwords must be the same',
+        path: ['passwordConfirmation'],
+    }),
+})
