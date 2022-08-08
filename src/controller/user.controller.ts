f1c15893cbcc430fb24a5fe74754388abd462f63
JakubMizera <kubamiz53@gmail.com>
2022-08-08 16:45:15 +0200

    user controller - omit password

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import {omit} from 'lodash'
 import { CreateUserInput } from "../schema/user.schema";
 import { createUser } from "../service/user.service";
 import logger from '../utils/logger';
@@ -10,7 +11,8 @@ export async function createUserHandler(
     try {
         //interface don't match, createdAt, updatedAt, comparePassword are not on createUser input
         const user = await createUser(req.body)
-        return res.send(user);
+        //(omit(user.toJSON(), 'password')); used to not send password  
+        return res.send(omit(user.toJSON(), "password"));
     } catch (error: any) {
         logger.error(error)
         return res.status(409).send(error)
